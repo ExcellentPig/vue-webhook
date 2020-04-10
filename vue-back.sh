7d42631efbaeed8d26c3dc0ff4ba502d4af913af
ExcellentPig <58382766+ExcellentPig@users.noreply.github.com>
2020-04-10 17:58:04 +0800

    init

--- vue-back.sh
+++ vue-back.sh
@@ -7,7 +7,7 @@ git clean -f
 echo "拉取最新代码"
 git pull origin master
 echo "开始执行构建"
-docker build -t vue-back .
+docker build -t vue-back:1.0 .
 echo "停止旧容器并删除旧容器"
 docker stop vue-back-container
 docker rm vue-back-container
